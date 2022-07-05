5c98b75c9f76ae0e80426da30d0c2b25cf65bd7f
souvik757 <88132246+souvik757@users.noreply.github.com>
2022-07-05 19:56:03 +0530

    Add files via upload

---
+++ func.js
@@ -0,0 +1,53 @@
+function displayTime() {
+    var dateTime = new Date();
+    var hrs = dateTime.getHours();
+    var min = dateTime.getMinutes();
+    var sec = dateTime.getSeconds();
+    var am = document.getElementById('am');
+
+    if (hrs >= 12) {
+        am.innerHTML = 'PM';
+    }
+    else {
+        am.innerHTML = 'AM';
+    }
+
+    if (hrs > 12) {
+        hrs = hrs - 12;
+    }
+
+    hrs = (hrs < 10) ? "0" + hrs : hrs;
+    min = (min < 10) ? "0" + min : min;
+    sec = (sec < 10) ? "0" + sec : sec;
+
+    document.getElementById('hours').innerHTML = hrs;
+    document.getElementById('mins').innerHTML = min;
+    document.getElementById('secs').innerHTML = sec;
+}
+function updateClock(){
+    var now = new Date();
+    var dname = now.getDay(),
+        mo = now.getMonth(),
+        dnum = now.getDate(),
+        yr = now.getFullYear();
+
+
+        Number.prototype.pad = function(digits){
+          for(var n = this.toString(); n.length < digits; n = 0 + n);
+          return n;
+        }
+
+        var months = ["January", "February", "March", "April", "May", "June", "July", "Augest", "September", "October", "November", "December"];
+        var week = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+        var ids = ["dayname", "month", "daynum", "year"];
+        var values = [week[dname], months[mo], dnum.pad(2), yr];
+        for(var i = 0; i < ids.length; i++)
+        document.getElementById(ids[i]).firstChild.nodeValue = values[i];
+  }
+
+  function initClock(){
+    updateClock();
+    window.setInterval("updateClock()", 1);
+  }
+
+setInterval(displayTime,10);
